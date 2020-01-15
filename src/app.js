78291daa9234336f35312418682e9aab1efb59b9
framista <adajab1@gmail.com>
2020-01-16 00:17:57 +0100

    Improved selecting examples - result must be unique

--- src/app.js
+++ src/app.js
@@ -11,15 +11,25 @@ function prepareDataMultiplyArray() {
     const data = []
     let positions = []
     for (let i = 0; i < cardsAmount / 2; i++) {
-        let number1 = Math.floor(Math.random() * 10) + 1;
-        let number2 = Math.floor(Math.random() * 10) + 1;
+        let numbers = randomNumbers(data)
         positions = randomPosition(positions)
         let variablePosition = positions[positions.length - 1]
         positions = randomPosition(positions)
         let resultPosition = positions[positions.length - 1]
-        data.push(new Multiply(number1, number2, variablePosition, resultPosition));
+        data.push(new Multiply(numbers, variablePosition, resultPosition));
     }
     return data;
+}
+
+function randomNumbers(data) {
+    while (true) {
+        let number1 = Math.floor(Math.random() * 10) + 1;
+        let number2 = Math.floor(Math.random() * 10) + 1;
+        let result = number1 * number2;
+        if (data.findIndex(d => d.result === result) == -1) {
+            return [number1, number2]
+        }
+    }
 }
 
 function randomPosition(positions) {
@@ -36,17 +46,17 @@ function setPosition(data, firstPosition) {
     let index = data.findIndex(d => d.variablePosition === firstPosition)
     if (index === -1) {
         index = data.findIndex(d => d.resultPosition === firstPosition)
-        return [index, data[index].variablePosition]
+        return [index, data[index].variablePosition, 1]
     }
-    return [index, data[index].resultPosition]
+    return [index, data[index].resultPosition, 0]
 
 }
 
 class Multiply {
-    constructor(number1, number2, variablePosition, resultPosition) {
-        this.number1 = number1;
-        this.number2 = number2;
-        this.result = number1 * number2;
+    constructor(numbers, variablePosition, resultPosition) {
+        this.number1 = numbers[0];
+        this.number2 = numbers[1];
+        this.result = this.number1 * this.number2;
         this.variablePosition = variablePosition
         this.resultPosition = resultPosition
     }
@@ -60,14 +70,21 @@ function clickedCard() {
     if (activeCards.length === 0) {
         activeCards[0] = activeCard;
         cardPosition = setPosition(gameData, activeCard)
-        cards[activeCard].innerHTML = gameData[cardPosition[0]].number1 + "•" + gameData[cardPosition[0]].number2
+        if (cardPosition[2] == 0)
+            cards[activeCard].innerHTML = gameData[cardPosition[0]].number1 + "•" + gameData[cardPosition[0]].number2
+        else
+            cards[activeCard].innerHTML = gameData[cardPosition[0]].result
+
         cards[activeCard].classList.add("element--showed")
         return
     } else {
         cards.forEach(card => card.removeEventListener("click", clickedCard))
         cards[activeCard].classList.add("element--showed")
-        let indexSecond = setPosition(gameData, activeCard)[0]
-        cards[activeCard].innerHTML = gameData[indexSecond].result
+        let cardPosition2 = setPosition(gameData, activeCard)
+        if (cardPosition2[2] == 0)
+            cards[activeCard].innerHTML = gameData[cardPosition2[0]].number1 + "•" + gameData[cardPosition2[0]].number2
+        else
+            cards[activeCard].innerHTML = gameData[cardPosition2[0]].result
         activeCards[1] = activeCard
         setTimeout(() => {
             if (activeCard === cardPosition[1]) {
